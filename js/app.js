33a1a73cce7ee4b6be1af8377237a9f4ffff63db
kar-thi <50635106+kar-thi@users.noreply.github.com>
2019-08-16 23:05:37 +0530

    landing start
    start

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  //   margin: 300,
+  nav: false,
+  navText: [
+    "<i class='fas fa-angle-left'></i>",
+    "<i class='fas fa-angle-right'></i>"
+  ],
+
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  },
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true
+});
